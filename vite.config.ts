fe578a67fbe7255de0df89c47186dc63b7a871b7
borisliao <borisliaos@gmail.com>
2025-06-07 16:13:12 -0400

    Fixed vite.svg not loading in GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [react()],
+  base: `/${process.env.npm_package_name}/`,
   build: {
     lib: {
       entry: "./src/main-web-component.tsx",
